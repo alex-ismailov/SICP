45c8546b29b814a13ea15f7c3a142a4a900c1610
alex-ismailov <alexander.is.dev@gmail.com>
2019-12-08 12:08:37 +0300

    old files deleted: 1.2.1* and 1.2.2*

--- 1.2.1 ex 1.11 - hexlet function_f/1.2.1 ex 1.11 - hexlet function_f (linear iterative proc).rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-(define (f n)
-  (define (iter a b c count)
-  (if(= count 0)
-     a
-     (iter (+ a (* 2 b) (* 3 c)) a b (- count 1))))
-  (if (< n 3)
-      n
-      (iter 2 1 0 (- n 2))))
--- 1.2.1 ex 1.11 - hexlet function_f/1.2.1 ex 1.11 - hexlet function_f (linear iterative process) - TEST-SIMPLE-CASE.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket
-(define (f n)
-  (define (iter a b c count)
-  (if(= count 0)
-     a
-     (iter (+ a b c) a b (- count 1))))
-  (iter 2 1 0 (- n 2)))
