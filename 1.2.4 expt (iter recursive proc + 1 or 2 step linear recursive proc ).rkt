0d2c71029f7b2a4b3085d3b81e491633ccec48e3
alex-ismailov <alexander.is.dev@gmail.com>
2019-12-08 11:17:07 +0300

    deleted: 1.2.4 expt* old files.rkt

--- 1.2.4 expt (iter recursive proc + 1 or 2 step linear recursive proc ).rkt
+++
@@ -1,8 +0,0 @@
-#lang racket
-(define (fast-expt b n)
-  (define (square n) (* n n))
-  (define (even? n) (= (remainder n 2) 0))
-  (cond((= n 0) 1)
-       ((even? n)
-        (square (fast-expt b (/ n 2))))
-       (else (* b (fast-expt b (- n 1))))))
--- 1.2.4 expt (linear iterative process).rkt
+++
@@ -1,7 +0,0 @@
-#lang racket
-(define (expt b n)
-  (define (iter acc n)
-    (if (= n 0)
-        acc
-        (iter (* acc b)(- n 1))))
-  (iter 1 n))
